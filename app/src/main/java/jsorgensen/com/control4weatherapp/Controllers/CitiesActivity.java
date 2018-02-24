b57d33bc0c71071639b9b6fb59204dc3669ed6a9
Mike Jorgensen <mikeljorgensen@gmail.com>
2018-02-23 18:53:54 -0700

    Initial commit

---
+++ app/src/main/java/jsorgensen/com/control4weatherapp/Controllers/CitiesActivity.java
@@ -0,0 +1,15 @@
+package jsorgensen.com.control4weatherapp.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jsorgensen.com.control4weatherapp.R;
+
+public class CitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
