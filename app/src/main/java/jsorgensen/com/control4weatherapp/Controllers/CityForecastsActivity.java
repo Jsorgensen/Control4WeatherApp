158cd9c1f36e75181a430074393366ee0b590492
Mike Jorgensen <mikeljorgensen@gmail.com>
2018-02-24 03:03:11 -0700

    creating cities forecast layout.

--- app/src/main/java/jsorgensen/com/control4weatherapp/Controllers/CityForecastsActivity.java
+++ app/src/main/java/jsorgensen/com/control4weatherapp/Controllers/CityForecastsActivity.java
@@ -1,13 +1,16 @@
 package jsorgensen.com.control4weatherapp.Controllers;
 
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.androidnetworking.AndroidNetworking;
 
 import jsorgensen.com.control4weatherapp.R;
+import jsorgensen.com.control4weatherapp.Views.CityForecastsFragment;
 
-public class CityForecastsActivity extends AppCompatActivity {
+public class CityForecastsActivity extends AppCompatActivity implements CityForecastsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,17 @@ public class CityForecastsActivity extends AppCompatActivity {
 
         AndroidNetworking.initialize(getApplicationContext());
 
+        cityForecastsFragment = new CityForecastsFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.flPanel, cityForecastsFragment)
+                .commit();
+    }
+
+    CityForecastsFragment cityForecastsFragment;
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
